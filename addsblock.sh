1a39e18518fa97b896a264acd22b8b6b85401937
xtremx@zinad.net <xtremx@zinad.net>
2018-05-10 16:17:02 +0200

    addsblock

--- addsblock.sh
+++ addsblock.sh
@@ -3,4 +3,4 @@ echo "Lets block all adds!"
 echo "Author:          Islam Taha "
 echo "Repository:      https://github.com/islamtaha12/addsblock"
 
-sudo curl -sSf "https://raw.githubusercontent.com/islamTaha12/addsblock/master/domainBlockList.txt" >> /etc/hosts
+curl -sSf "https://raw.githubusercontent.com/islamTaha12/addsblock/master/domainBlockList.txt" >> /etc/hosts
