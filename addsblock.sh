f859552367ae3324efb344fba8862851f366e53e
islamTaha12 <tahai@greenitglobe.com>
2018-05-14 11:42:39 +0200

    Update addsblock.sh

--- addsblock.sh
+++ addsblock.sh
@@ -1,6 +1,6 @@
 
 echo "Lets block all adds!"
 echo "Author:          Islam Taha "
-echo "Repository:      https://github.com/islamtaha12/addsblock"
+echo "Repository:      https://github.com/islamtaha12/adsblock"
 
-curl -sSf "https://raw.githubusercontent.com/islamTaha12/addsblock/master/domainBlockList.txt" >> /etc/hosts
+curl -sSf "https://raw.githubusercontent.com/islamTaha12/adsblock/master/domainBlockList.txt" >> /etc/hosts
